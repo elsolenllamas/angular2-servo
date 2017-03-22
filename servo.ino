97122d31b11e2a679ff7444842729e431ed945d7
elsolenllamas <elsolenllamas@gmail.com>
2017-03-22 20:28:24 -0300

    added ino file

---
+++ servo.ino
@@ -0,0 +1,41 @@
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
+#include <Servo.h>
+
+BridgeServer server;
+Servo servo1; 
+
+void setup() {
+  servo1.attach(5); // pin number where servo is connected
+  Bridge.begin();
+  server.listenOnLocalhost();
+  server.begin();
+}
+
+void loop() {
+  BridgeClient client = server.accept();
+  if (client) {
+    process(client);
+    client.stop();
+  }
+  delay(15);
+}
+
+void process(BridgeClient client) {
+  String command = client.readStringUntil('/');
+  if (command == "servo") {
+    servoCommand(client);
+  }
+}
+
+void servoCommand(BridgeClient client) {
+  int pin, value;
+  pin = client.parseInt();
+  if (client.read() == '/') {
+    value = client.parseInt();
+    if (pin == 5)
+      servo1.write(value);
+  }
+}
+
